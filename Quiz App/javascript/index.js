c351e7bc2d275d7d28588cc36bd4f3f46468fcf1
Alex <adt71@cox.net>
2017-11-29 21:52:05 -0700

    Quiz App finished

--- Quiz App/javascript/index.js
+++ Quiz App/javascript/index.js
@@ -1,33 +1,9 @@
-// function beginQuiz() {
-//
-//   document.getElementById('start').style.display = "none";
-// document.getElementById('question-one').style.display = 'block';
-//
-// }
-//
-// function checkAnswer() {
-// let question1 = document.quiz.q_one.value;
-//   if (question1 == "Yuri Gagarin") {
-//       alert("Correct! 1 out of 10 so far.");
-//   } else {
-//       alert("Wrong! 0 out of 10 so far");
-//   }
-// }
-//
-// function nextQuestion() {
-//
-//   let x = document.getElementById('question-two');
-//   let y = document.getElementById('question-one');
-//     if(x.style.display === "none" && y.style.display !== "none") {
-//       x.style.display = "block";
-//       y.style.display = "none"
-//     } else {
-//       x.style.display = "none";
-//     }
-//
-// }
-
-
+/*
+*  Alex THomas
+*  thomasad71@gmail.com
+*  29 NOV 17
+*
+* */
 
 const quizArray = [
   {
@@ -52,220 +28,244 @@ const quizArray = [
       ],
       'questionNumber': 1,
       'correctAnswer': 1
-  },
+    },
 
     {
        'question':'In what year did the first human orbit the earth?',
             'answer':  [
-        {
-        value: 1975,
-        number: 1
-        },
-        { value: 1961,
-        number: 2
-        },
-        { value:1969,
-        number: 3
-        },
-        { value:1989,
-        number: 4
-        },
+          {
+            value: 1975,
+            number: 1
+          },
+          {
+            value: 1961,
+            number: 2
+          },
+          {
+            value:1969,
+            number: 3
+          },
+          {
+            value:1989,
+            number: 4
+          },
         ],
         'questionNumber': 2,
         'correctAnswer': 2
       },
 
 
-
       {
         'question':'What was the name of America\'s first manned space program?',
           'answer': [
       {
-      value: 'Gemini',
-      number: 1
+          value: 'Gemini',
+          number: 1
       },
-      { value: 'Apollo',
-      number: 2
+      {
+          value: 'Apollo',
+          number: 2
       },
-      { value: 'Mercury',
-      number: 3
+      {
+          value: 'Mercury',
+          number: 3
       },
-      { value: 'Odin',
-      number: 4
+      {
+          value: 'Odin',
+          number: 4
       },
       ],
-              'questionNumber': 3,
-              'correctAnswer': 3
-            },
+        'questionNumber': 3,
+        'correctAnswer': 3
+      },
 
       {
         'question': ' In what year did Man first land on the moon?',
           'answer': [
       {
-      value: '1972',
-      number: 1
+          value: '1972',
+          number: 1
       },
-      { value: '1973',
-      number: 2
+      {
+          value: '1973',
+          number: 2
       },
-      { value: '1969',
-      number: 3
+      {
+          value: '1969',
+          number: 3
       },
-      { value: '1944',
-      number: 4
+      {
+          value: '1944',
+          number: 4
       },
       ],
-              'questionNumber': 4,
-              'correctAnswer': 3
-            },
+          'questionNumber': 4,
+          'correctAnswer': 3
+        },
 
       {
         'question': 'Who said, "One small step for Man, one giant leap for Mankind"?',
           'answer':[
       {
-      value: 'John Glenn',
-      number: 1
+          value: 'John Glenn',
+          number: 1
       },
-      { value: 'Gus Grissom',
-      number: 2
+      {
+          value: 'Gus Grissom',
+          number: 2
       },
-      { value: 'Neil Armstrong',
-      number: 3
+      {
+          value: 'Neil Armstrong',
+          number: 3
       },
-      { value: 'Scooby Doo',
-      number: 4
+      {
+          value: 'Scooby Doo',
+          number: 4
       },
       ],
-              'questionNumber': 5,
-              'correctAnswer': 3
-            },
+        'questionNumber': 5,
+        'correctAnswer': 3
+          },
 
       {
         'question': 'Who was the first Man to step onto the moon\'s surface?',
           'answer':[
       {
-      value: 'Neil Armstrong',
-      number: 1
+          value: 'Neil Armstrong',
+          number: 1
       },
-      { value: 'Vladimir Putin',
-      number: 2
+      {
+          value: 'Vladimir Putin',
+          number: 2
       },
-      { value: 'John Glenn',
-      number: 3
+      {
+          value: 'John Glenn',
+          number: 3
       },
-      { value: 'Yuri Gagarin',
-      number: 4
+      {
+          value: 'Yuri Gagarin',
+          number: 4
       },
       ],
-              'questionNumber': 6,
-              'correctAnswer': 1
-            },
+        'questionNumber': 6,
+        'correctAnswer': 1
+          },
 
       {
         'question': 'Who was the first female astronaut?',
           'answer': [
       {
-      value: 'Sally Ride',
-      number: 1
+          value: 'Sally Ride',
+          number: 1
       },
-      { value: 'Valentina Tereshkova',
-      number: 2
+      {
+          value: 'Valentina Tereshkova',
+          number: 2
       },
-      { value: 'Jane Smith',
-      number: 3
+      {
+          value: 'Jane Smith',
+          number: 3
       },
-      { value: 'Lady Gaga',
-      number: 4
+      {
+          value: 'Lady Gaga',
+          number: 4
       },
       ],
-              'questionNumber': 7,
-              'correctAnswer': 2
-            },
+        'questionNumber': 7,
+        'correctAnswer': 2
+      },
 
       {
         'question': 'Who was the first mom to travel to space?',
           'answer': [
       {
-      value: 'Valentina Tereshkova',
-      number: 1
+          value: 'Valentina Tereshkova',
+          number: 1
       },
-      { value: 'Sally Ride',
-      number: 2
+      {
+          value: 'Sally Ride',
+          number: 2
       },
-      { value: 'Anna Fisher',
-      number: 3
+      {
+          value: 'Anna Fisher',
+          number: 3
       },
-      { value: 'Lady Gaga',
-      number: 4
+      {
+          value: 'Lady Gaga',
+          number: 4
       },
       ],
-              'questionNumber': 8,
-              'correctAnswer': 3
-            },
+        'questionNumber': 8,
+        'correctAnswer': 3
+      },
 
       {
         'question': 'What was the name of the first spacecraft on Mars?',
           'answer': [
       {
-      value: 'Mars 3',
-      number: 1
+          value: 'Mars 3',
+          number: 1
       },
-      { value: 'Viking 1',
-      number: 2
+      {
+          value: 'Viking 1',
+          number: 2
       },
-      { value: 'Good Luck 1',
-      number: 3
+      {
+          value: 'Good Luck 1',
+          number: 3
       },
-      { value: 'Good Bye 1',
-      number: 4
+      {
+          value: 'Good Bye 1',
+          number: 4
       },
       ],
-              'questionNumber': 9,
-              'correctAnswer': 1
+        'questionNumber': 9,
+        'correctAnswer': 1
             },
 
       {
         'question': 'In what year did the first Mars landing occur?',
           'answer': [
       {
-      value: 1975,
-      number: 1
+          value: 1975,
+          number: 1
       },
-      { value: 1966,
-      number: 2
+      {
+          value: 1966,
+          number: 2
       },
-      { value: 1969,
-      number: 3
+      {
+          value: 1969,
+          number: 3
       },
-      { value: 1971,
-      number: 4
+      {
+          value: 1971,
+          number: 4
       },
       ],
-              'questionNumber': 10,
-              'correctAnswer': 1
+        'questionNumber': 10,
+        'correctAnswer': 1
             },
-    ]
+    ];
 
-    var correct = 0
+   var answerArray = [];
+    var correct = 0;
       function correctGuess() {
         correct += 1;
-
       }
 
-      function rightOrWrong(questionNumber, userAnswer) {
+
+    function rightOrWrong(questionNumber, userAnswer) {
       var questionObject = questionNumber - 1;
       var obj = quizArray[questionObject];
-
-      console.dir(obj);
-      console.log(userAnswer);
       if(userAnswer == obj.correctAnswer) {
+        correctGuess();
+        $('#amount').html(correct);
         $('#display-right-or-wrong').html('Correct!');
-        // console.log('correct');
-      } else {
+        } else {
         $('#display-right-or-wrong').html('Wrong!');
-      }
-
-      }
+        }
+    }
 
 
     $(function() {
@@ -273,53 +273,49 @@ const quizArray = [
         $('.container').hide(1000);
         $('.results').hide();
         $('.show-results').hide();
-        $(' #count ').html(pageNumber); //<------renders question count
+        $(' #count ').html(pageNumber);
         $('#quiz').show(function() {
           showQuestion(0);
         });
       });
     });
 
-
-
+  var clickCount = 0;
   var pageNumber = 1;
+  function clickTally() {
+        clickCount += 1;
+        if(pageNumber === 10) {
+          $('.js-next').hide();
+          $('#display-right-or-wrong').hide();
+          results();
+          console.log('bananas');
+        }
+    }
+
     $(function() {
       $('.js-next').on('click', function(event) {
         event.preventDefault();
-        showQuestion(pageNumber);
-        pageNumber++;
+        clickTally();
+          pageNumber++;
+        showQuestion(clickCount);
+
         $('#display-right-or-wrong').html('');
-        $(' #count ').html(pageNumber); //<------renders question count
-        if(pageNumber == 10) {
-        results();
 
-      };
-
+        if(pageNumber < 11) {
+            $(' #count ').html(pageNumber);
+        }
       });
     });
 
-    function answerRequired() {
+
+   function check() {
       $('.js-next').attr('disabled', true);
-      $('input[type="radio"]').on('click', function() {
-        $('.js-next').attr('disabled', false);
-      });
     }
 
-    //
-    // `$("input[type=submit]").attr("disabled", "disabled");`
-    //
-    //
-    // [6:21]
-    // `$("input[type=submit]").attr("disabled", "enabled");`
-    //
-    //
-    // [6:21]
-    // `<button disabled="disabled">Next</button>`
-    //
-    //
-    // [6:22]
-    // `<button disabled="enabled">Next</button>`
-    // need functionality for one choice only
+    function answerRequired() {
+      $('.js-next').attr('disabled', false);
+    }
+
 
     function results() {
       $('.results').show(1000);
@@ -328,23 +324,28 @@ const quizArray = [
       $('.show-results').show(1000);
       $('#restart').show();
       });
-    };
+    }
 
 
     function showQuestion(bananas) {
-      generateAnswersHTML(quizArray[bananas].answer);
-      generateQuestionHTML(quizArray[bananas].question);
-      $('input[type="radio"]').on('click', function() {
-      rightOrWrong(quizArray[bananas].questionNumber, $(this).val());
-      })
+      console.log(pageNumber);
+      if(pageNumber < 11) {
+        generateAnswersHTML(quizArray[bananas].answer);
+        generateQuestionHTML(quizArray[bananas].question);
+        $('input[type="radio"]').on('click', function() {
+          $('input[type="radio"]').attr('disabled', true);
+          rightOrWrong(quizArray[bananas].questionNumber, $(this).val());
+          answerRequired();
+        });
+        check();
+        }
     };
-
 
 
     function generateQuestionHTML(question) {
-      var  questionNumber = '<h2>' + question + '</h2>'
+      var  questionNumber = '<h2>' + question + '</h2>';
       $('.js-question').html(questionNumber);
-    };
+    }
 
 
     function generateAnswersHTML(answers) {
@@ -352,6 +353,12 @@ const quizArray = [
       for(i = 0; i < answers.length; i++) {
         let item = answers[i];
         answerList += '<input type="radio" name="q-one" value="' + item.number + '">' + item.value + '';
-        };
+        }
       $('.js-answer').html(answerList);
-    };
+
+    }
+
+
+    $('#restart').on('click', function() {
+      location.reload();
+    })
